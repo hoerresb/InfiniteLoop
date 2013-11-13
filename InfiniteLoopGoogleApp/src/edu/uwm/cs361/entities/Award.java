8ea8cab08e32d8597f1b11fc90f5182e43212047
KYoung91 <kevinpyoung@ameritech.net>
2013-11-13 03:17:26 -0600

    Update Award.java

--- InfiniteLoopGoogleApp/src/edu/uwm/cs361/entities/Award.java
+++ InfiniteLoopGoogleApp/src/edu/uwm/cs361/entities/Award.java
@@ -8,7 +8,7 @@ import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
 
 import com.google.appengine.api.datastore.Key;
-
+@PersistenceCapable
 public class Award {
 	
 	@PrimaryKey
