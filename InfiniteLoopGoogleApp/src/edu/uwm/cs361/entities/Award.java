ff0d1163a83ade089fa7050e393bbf94d6bd151b
nbonnet13 <thebonnet09@yahoo.com>
2013-11-13 12:35:59 -0600

    Added import that was causing errors

--- InfiniteLoopGoogleApp/src/edu/uwm/cs361/entities/Award.java
+++ InfiniteLoopGoogleApp/src/edu/uwm/cs361/entities/Award.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
 
