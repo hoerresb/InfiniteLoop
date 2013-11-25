aa6c2f4e8dbe181c08f16a0dc3131a338be1826b
nbonnet13 <thebonnet09@yahoo.com>
2013-11-25 17:05:59 -0600

    Made errors work

--- InfiniteLoopGoogleApp/src/edu/uwm/cs361/LoginServlet.java
+++ InfiniteLoopGoogleApp/src/edu/uwm/cs361/LoginServlet.java
@@ -38,57 +38,52 @@ public class LoginServlet extends HttpServlet {
 			errors.add("Password is required.");
 		}
 		
-		
-		
-		try{
-			req.setAttribute("errors", errors);
-			resp.setContentType("text/html");
-
-		List<Admin> adm = (List<Admin>) pm.newQuery(Admin.class).execute();
+		if(errors.size() < 1) {
+			try{
+				req.setAttribute("errors", errors);
+				resp.setContentType("text/html");
 	
-		List<Student> Student = (List<Student>) pm.newQuery(Student.class).execute();
-		
-		List<Teacher> Teacher = (List<Teacher>) pm.newQuery(Teacher.class).execute();
-		
-		if(adm.size()>0){
-		for(int i = 0 ; i < adm.size(); i ++){
-			if(adm.get(i).getPassword().equals(password) && adm.get(i).getUsername().equals(username)){
-				Cookie c = new Cookie("Adminname", req.getParameter("username"));
-				resp.addCookie(c);
-				resp.sendRedirect("/AdminHome");
+				List<Admin> adm = (List<Admin>) pm.newQuery(Admin.class).execute();
+			
+				List<Student> Student = (List<Student>) pm.newQuery(Student.class).execute();
+				
+				List<Teacher> Teacher = (List<Teacher>) pm.newQuery(Teacher.class).execute();
+				
+				if(adm.size()>0){
+				for(int i = 0 ; i < adm.size(); i ++){
+					if(adm.get(i).getPassword().equals(password) && adm.get(i).getUsername().equals(username)){
+						Cookie c = new Cookie("Adminname", req.getParameter("username"));
+						resp.addCookie(c);
+						resp.sendRedirect("/AdminHome");
+					}
+				}}
+				
+				if(Student.size()>0){
+				for(int i = 0 ; i < Student.size(); i ++){
+					if(Student.get(i).getPassword().equals(password) && Student.get(i).getUsername().equals(username)){
+						Cookie student = new Cookie("Studentname", req.getParameter("username"));
+						resp.addCookie(student);
+						resp.sendRedirect("/StudentHome");
+					}
+				}}
+				
+				if(Teacher.size()>0){
+				for(int i = 0 ; i < Teacher.size(); i ++){
+					if(Teacher.get(i).getPassword().equals(password) && Teacher.get(i).getUsername().equals(username)){
+						Cookie teacher = new Cookie("Teachername", req.getParameter("username"));
+						resp.addCookie(teacher);
+						resp.sendRedirect("/TeacherHome");
+					}
+				}}
+			}catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				pm.close();
 			}
-		}}
-		
-		if(Student.size()>0){
-		for(int i = 0 ; i < Student.size(); i ++){
-			if(Student.get(i).getPassword().equals(password) && Student.get(i).getUsername().equals(username)){
-				Cookie student = new Cookie("Studentname", req.getParameter("username"));
-				resp.addCookie(student);
-				resp.sendRedirect("/StudentHome");
-			}
-		}}
-		
-		if(Teacher.size()>0){
-		for(int i = 0 ; i < Teacher.size(); i ++){
-			if(Teacher.get(i).getPassword().equals(password) && Teacher.get(i).getUsername().equals(username)){
-				Cookie teacher = new Cookie("Teachername", req.getParameter("username"));
-				resp.addCookie(teacher);
-				resp.sendRedirect("/TeacherHome");
-			}
-		}}
-		else{
+		} else {
+			req.setAttribute("errors", errors);
 			req.getRequestDispatcher("/login.jsp").forward(req, resp);	
 		}
-			
-		}catch (ServletException e) {
-			e.printStackTrace();
-		} finally {
-			pm.close();
-		}
-		
-		
-		
-		
 	}
 
 
--- InfiniteLoopGoogleApp/war/login.jsp
+++ InfiniteLoopGoogleApp/war/login.jsp
@@ -1,4 +1,6 @@
 <%@ page contentType="text/html; charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
 
 <%@ page import="java.util.List" %>
 <%@ page import="javax.jdo.JDOHelper" %>
@@ -14,6 +16,7 @@
 							<div id="container"> 
 								<div id="banner"></div> 
 								<div id="content">
+								<%@include file='/templates/error.html'%>
 									<span class="title">Log In</span>
 									<form id="form-id" method="POST" action='/login'> 
 										<label for="username-id">Username:</label>
