4f1d5e4bc4c9764e0d2fa7d808ca2c28426b64ee
KYoung91 <kevinpyoung@ameritech.net>
2013-12-01 16:10:31 -0600

    Update IssueAward2Servlet.java

--- InfiniteLoopGoogleApp/src/edu/uwm/cs361/IssueAward2Servlet.java
+++ InfiniteLoopGoogleApp/src/edu/uwm/cs361/IssueAward2Servlet.java
@@ -18,3 +18,104 @@ import edu.uwm.cs361.entities.Award;
 import edu.uwm.cs361.entities.Student;
 import edu.uwm.cs361.entities.Teacher;
 import edu.uwm.cs361.entities.Course;
+@SuppressWarnings("serial")
+public class IssueAwardServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException        {
+        
+        String courseid = null;
+
+        Cookie[] cookies = req.getCookies();
+        
+        if(cookies == null){
+                resp.sendRedirect("/login.jsp");
+        }
+        if (cookies != null) {
+        	for (Cookie c : cookies) {
+
+                if(c.getName().equals("Coursename")){
+                               courseid = c.getValue();
+                }
+                
+                if(c.getName().isEmpty()){
+                     resp.sendRedirect("/login.jsp");
+                }
+                               
+            }
+        }
+        
+        Course course = getCourse(courseid);
+        
+        req.setAttribute("award_students", getStudents());
+
+        req.setAttribute("course_awards", getAwards(course));
+        req.getRequestDispatcher("IssueAward2.jsp").forward(req, resp);
+    }
+	
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException	{
+		
+		PersistenceManager pm = getPersistenceManager();
+
+		try {
+				Cookie c = new Cookie("Coursename", req.getParameter("award_courses"));
+                resp.addCookie(c);
+
+			
+			resp.sendRedirect("Awards.jsp");
+			
+		} finally {
+			pm.close();
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+    private List<Student> getStudents() {
+            PersistenceManager pm = getPersistenceManager();
+            List<Student> students = new ArrayList<Student>();
+            try {
+                    students = (List<Student>) pm.newQuery(Student.class).execute();
+            } finally {
+                    pm.close();
+            }
+            return students;
+    }
+	
+	@SuppressWarnings("unchecked")
+    private Course getCourse(String courseid) {
+            PersistenceManager pm = getPersistenceManager();
+            List<Course> courses = new ArrayList<Course>();
+            Course course = null;
+            try {
+                    courses = (List<Course>) pm.newQuery(Course.class).execute();
+                    long courseId = Long.parseLong(courseid);
+                    for (Course c : courses) {
+                            if (c.getCourse_id().getId() == courseId) {
+                                    course = c;
+                            }
+                    }
+            } finally {
+                    pm.close();
+            }
+            return course;
+    }
+	
+	   private Set<Award> getAwards(Course course) {
+           PersistenceManager pm = getPersistenceManager();
+           Set<Award> awards = new HashSet<Award>();
+           try {
+                   if (course != null) {
+                           if (course.getAwards() != null) {
+                                   awards = course.getAwards();
+                           }
+                   }
+           } finally {
+                   pm.close();
+           }
+           return awards;
+   }
+	
+	private PersistenceManager getPersistenceManager() {
+		return JDOHelper.getPersistenceManagerFactory("transactions-optional").getPersistenceManager();
+	}
+}
