96c19cfc572d29002b5d9b4099d8e10fe5022455
cjsampon <cjsampon@yahoo.com>
2013-12-04 20:16:04 -0600

    StudentChargesFactoryTest done
    -Student charges factory test now works
    -Also removed unused test

--- InfiniteLoopGoogleApp/src/edu/uwm/cs361/tests/StudentChargesFactoryTest.java
+++ InfiniteLoopGoogleApp/src/edu/uwm/cs361/tests/StudentChargesFactoryTest.java
@@ -1,30 +1,17 @@
 package edu.uwm.cs361.tests;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 
-import edu.uwm.cs361.entities.Charge;
-import edu.uwm.cs361.entities.Student;
+import edu.uwm.cs361.entities.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
-import javax.jdo.JDOHelper;
-import javax.jdo.PersistenceManager;
-import javax.jdo.Query;
+import javax.jdo.*;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 public class StudentChargesFactoryTest {
 	private final LocalServiceTestHelper helper = new LocalServiceTestHelper(
@@ -34,7 +21,6 @@ public class StudentChargesFactoryTest {
 	private List<Student> students = new ArrayList<Student>();
 
 	@Before
-	@SuppressWarnings({ "unchecked", "deprecation" })
 	public void setUp() {
 		helper.setUp();
 		pm = getPersistenceManager();
@@ -52,18 +38,17 @@ public class StudentChargesFactoryTest {
 	
 	@Test
 	public void testOneStudents() {
-		Charge[] charges = {new Charge(12, new Date(2013,11,31), ""), new Charge(15, new Date(2013,11,31), "") , new Charge(18, new Date(2013,11,31), "")};
-		Student user = new Student("student", "student", "Student_fn", "Student_ln", "student@student.com", "111-111-1111", charges);
+		Student user = new Student("student", "student", "Student_fn", "Student_ln", "student@student.com");
 		students.add(user);
 		assertEquals(1, students.size());
 	}
 	
+	@SuppressWarnings("deprecation")
 	@Test
 	public void testAddCharges () {
 		double amount = 0;
 		Date deadline = new Date();
-		Charge[] charges = {};
-		Student student = new Student("student", "student", "Student_fn", "Student_ln", "student@student.com", "111-111-1111", charges);
+		Student student = new Student("student", "student", "Student_fn", "Student_ln", "student@student.com");
 		students.add(student);
 		
 		student.getCharges().add(new Charge(6, new Date(2013,8,12), ""));
