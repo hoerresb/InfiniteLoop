5fc71d7140d0b3b8f835c624888a85fba124f41c
KYoung91 <kevinpyoung@ameritech.net>
2013-12-07 20:07:37 -0600

    Create CreateAwardFactory.java

---
+++ InfiniteLoopGoogleApp/src/edu/uwm/cs361/factories/CreateAwardFactory.java
@@ -0,0 +1,41 @@
+package edu.uwm.cs361.factories;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import edu.uwm.cs361.entities.*;
+
+public class CreateAwardFactory {
+	private List<String> errors = new ArrayList<String>();
+	
+	public Award createAward(Course course, String name, String description) {
+		
+		if (name.isEmpty()) {
+			errors.add("Please enter an award name.");
+		}
+		if(description.isEmpty()){
+			errors.add("Please enter an award description.");
+		}
+		if (course == null) {
+				errors.add("Please specify a course.");
+		} 
+		
+		
+		if(hasErrors()) {
+			return null;
+		} else {
+			Award a = new Award(name, description);
+			course.getAwards().add(a);
+			return a;
+		}
+	}
+	
+	public List<String> getErrors() {
+		return errors;
+	}
+	
+	public boolean hasErrors() {
+		return !errors.isEmpty();
+	}
+}
