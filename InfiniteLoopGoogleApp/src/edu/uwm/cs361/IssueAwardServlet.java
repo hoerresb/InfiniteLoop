86bc5ff7160ea6a16eec4d307057adbc781e8ee4
KYoung91 <kevinpyoung@ameritech.net>
2013-12-10 01:20:39 -0600

    Update IssueAwardServlet.java

--- InfiniteLoopGoogleApp/src/edu/uwm/cs361/IssueAwardServlet.java
+++ InfiniteLoopGoogleApp/src/edu/uwm/cs361/IssueAwardServlet.java
@@ -1,13 +1,9 @@
 package edu.uwm.cs361;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.ArrayList;
 
 import javax.jdo.PersistenceManager;
 import javax.servlet.ServletException;
-import javax.jdo.Query;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -42,17 +38,38 @@ public class IssueAwardServlet extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException	{
 		PersistenceManager pm = PersistenceFactory.getPersistenceManager();
 		try{
-			String awardID = req.getParameter("award_options");
-			String studentID = req.getParameter("student_options");
-			Student student = (Student) pm.getObjectById(Student.class,Long.parseLong(studentID));
-			Award award = (Award) pm.getObjectById(Award.class,Long.parseLong(awardID));
+			//get student id's, parse id's, add students to array list
+			String[] studentIDs = req.getParameterValues("student_options");
+			ArrayList<Student> students = new ArrayList<Student>();
+			for(String s : studentIDs){
+				students.add((Student) pm.getObjectById(Student.class,Long.parseLong(s)));
+			}
+			//get award id's, parse id's, add awards to array list
+			String[] awardIDs = req.getParameterValues("award_options");
+			ArrayList<Award> awards = new ArrayList<Award>();
+			for(String s : awardIDs){
+				awards.add((Award) pm.getObjectById(Award.class,Long.parseLong(s)));
+			}
 
-			IssueAwardFactory fact = new IssueAwardFactory();
-			boolean issued = fact.issueAward(student, award);
-			if(issued)
-				req.setAttribute("success", "Award given successfully.");
+			IssueAwardFactory fact;
+			boolean success = true;//overall success
+			boolean issued;        //success of individual attempts to give an award 
+			for(Student s : students){//loop through students and awards, giving each award to each student
+				for(Award a : awards){
+					fact = new IssueAwardFactory();
+					issued = fact.issueAward(s, a);
+					if(!issued){
+						success = false;
+						req.setAttribute("errors",  fact.getErrors());
+					}
+				}
+			}
+			
+			
+			if(success)
+				req.setAttribute("success", "Award(s) given successfully.");
 			else
-				req.setAttribute("errors", fact.getErrors());
+				req.setAttribute("success",  "Sorry, one or more attempts to give an award failed");
 					
 			req.setAttribute("student_options", course.getStudents());
 			req.setAttribute("award_options", course.getAwards());
@@ -67,4 +84,3 @@ public class IssueAwardServlet extends HttpServlet {
 		
 	}
 }
-
