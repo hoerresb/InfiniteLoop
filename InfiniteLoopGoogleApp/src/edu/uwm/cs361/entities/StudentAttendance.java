249f347e02671a56024ba1b927fa898cd4c67812
hoerresb <brandonhoerres@gmail.com>
2013-12-14 11:32:11 -0600

    forgot this one thing
    student attendance etnities to  be added to a specific course

---
+++ InfiniteLoopGoogleApp/src/edu/uwm/cs361/entities/StudentAttendance.java
@@ -0,0 +1,54 @@
+package edu.uwm.cs361.entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.datastore.Key;
+
+public class StudentAttendance {
+	
+	@Persistent
+	int WeekDate;
+	
+	@Persistent
+	Student student;
+	
+	@Persistent
+	List<String> present;
+	
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Key attendance_id;
+	
+	//precence.put("th", true)
+	
+	public StudentAttendance(List<String> p, int date, Student s){
+		this.present = p;
+		this.WeekDate = date;
+		this.student = s;
+	}
+	
+	public Key getAttendance_id(){
+		return attendance_id;
+	}
+	
+	public int getDate(){
+		return WeekDate;
+	}
+	
+	public List<String> getList(){
+		return present;
+	}
+	
+	public Student get_a_Student(){
+		return student;
+	}
+	
+
+}
